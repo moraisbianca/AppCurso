8123a19a43bd4b3ad8b17d525feeaf8e832f05c0
moraisbianca <biancacamilamorais@gmail.com>
2022-10-20 16:02:33 -0300

    Continuação de estilização

--- ProjetoCurso/ProjetoCurso/View/PrimeiroAno.xaml.cs
+++ ProjetoCurso/ProjetoCurso/View/PrimeiroAno.xaml.cs
@@ -24,13 +24,24 @@ namespace ProjetoCurso.View
             {
                 var c = new Componente
                 {
-                    Nome = "",
-                    CargaHoraria = 0,
-                    Atribuicoes = "",
-                    ValoresAtitudes = "",
-                    Competencias = "",
-                    Habilidades = "", 
-                    BasesTecnologicas = ""
+                    Nome = "Fundamentos da Informática",
+                    Atribuicoes = "Operar sistemas computacionais.",
+                    ValoresAtitudes = "Desenvolver a criticidade.\n" + 
+                                      "Incentivar comportamentos éticos.\n" + 
+                                      "Promover ações que considerem o respeito às normas estabelecidas.",
+                    Competencias = "Articular conhecimentos de sistemas computacionais.\n" + 
+                                   "Distinguir sistemas computacionais.",
+                    Habilidades = "Distinguir arquiteturas de sistemas de hardware e software.\n" + 
+                                  "Executar comandos em interface de linha de comando.\n" + 
+                                  "Utilizar sistemas computacionais.", 
+                    BasesTecnologicas = "Conceitos básicos de Tecnologia da Informação\n" +
+                                        "Laboratório em sistemas operacionais\n" +
+                                        "Manipulação de arquivos\n" +
+                                        "Pipes, redirecionamentos e filtros (>, >>, |, head, tail, sort)\n" +
+                                        "Permissões (chmod)\n" +
+                                        "Execução de comandos em lote (#!, echo)\n" +
+                                        "Processos (CTRL+C, kill, os)\n" +
+                                        "Utilização de Interface Gráfica\n"
 
                 };
 
@@ -48,13 +59,29 @@ namespace ProjetoCurso.View
             {
                 var c = new Componente
                 {
-                    Nome = "",
-                    CargaHoraria = 0,
-                    Atribuicoes = "",
-                    ValoresAtitudes = "",
-                    Competencias = "",
-                    Habilidades = "",
-                    BasesTecnologicas = ""
+                    Nome = "Programação de Algoritmos",
+                    Atribuicoes = "Verificar usabilidade no desenvolvimento de programas.\n" + 
+                                  "Realizar versionamento no desenvolvimento de programas.\n" + 
+                                  "Desenvolver programas de computador, utilizando princípios de boas práticas.\n" + 
+                                  "Implementar algoritmos em linguagem de programação, utilizando ambientes de desenvolvimento de acordo com as necessidades.\r\n",
+                    ValoresAtitudes = "Incentivar a criatividade.\n" + 
+                                      "Incentivar atitudes de autonomia.\n" + 
+                                      "Incentivar comportamentos éticos.\n" +
+                                      "Fortalecer a persistência e o interesse na resolução de situações-problema.",
+                    Competencias = "Implementar algoritmos de programação.\n" + 
+                                   "Desenvolver sistemas aplicando princípios e paradigmas de programação.",
+                    Habilidades = "Elaborar algoritmos.\n" + 
+                                  "Codificar programas, utilizando técnica de programação estruturada.\n" + 
+                                  "Depurar e versionar programas, utilizando ambiente de desenvolvimento integrado.",
+                    BasesTecnologicas = "Princípios de programação." +
+                                        "Comandos da linguagem de programação." +
+                                        "Programação estruturada." +
+                                        "Ferramentas para o desenvolvimento." +
+                                        "Verificação e depuração de código." +
+                                        "Programação modular." +
+                                        "Tipos de dados estruturados." +
+                                        "Versionamento e colaboração." +
+                                        "Práticas de programação."
 
                 };
 
@@ -73,19 +100,18 @@ namespace ProjetoCurso.View
                 var c = new Componente
                 {
                     Nome = "Programação Web I",
-                    CargaHoraria = 80,
-                    Atribuicoes = "Desenvolver sites para web",
-                    ValoresAtitudes = "Incentivar a criatividade" + 
-                                      "Estimular a organização" +
-                                      "Fortalecer a persistência na resolução de situações-problema",
-                    Competencias = "Desenvolver páginas para a internet",
-                    Habilidades = "Construir páginas para internet utilizando linguagem de marcação de texto" +
-                                  "Utilizar linguagem de script para web" +
-                                  "aplicar formatação através dee folhas de estilo",
-                    BasesTecnologicas = " - Conceitos de desenvolvimento para a web" +
-                                        " - Linguagem de marcação para a web (HTML)" +
-                                        " - Documento HTML mínimo, tags, atributos e conteúdo" +
-                                        " - Estilos em Cascata"
+                    Atribuicoes = "Desenvolver sites para web.",
+                    ValoresAtitudes = "Incentivar a criatividade, \n" + 
+                                      "Estimular a organização, \n" +
+                                      "Fortalecer a persistência na resolução de situações-problema.",
+                    Competencias = "Desenvolver páginas para a internet.",
+                    Habilidades = "Construir páginas para internet utilizando linguagem de marcação de texto, \n" +
+                                  "Utilizar linguagem de script para web, \n" +
+                                  "Aplicar formatação através de folhas de estilo.",
+                    BasesTecnologicas = " - Conceitos de desenvolvimento para a web. \n" +
+                                        " - Linguagem de marcação para a web (HTML). \n" +
+                                        " - Documento HTML mínimo, tags, atributos e conteúdo. \n" +
+                                        " - Estilos em Cascata."
 
 
                 };
@@ -104,13 +130,32 @@ namespace ProjetoCurso.View
             {
                 var c = new Componente
                 {
-                    Nome = "",
-                    CargaHoraria = 0,
-                    Atribuicoes = "",
-                    ValoresAtitudes = "",
-                    Competencias = "",
-                    Habilidades = "",
-                    BasesTecnologicas = ""
+                    Nome = "Banco de Dados I",
+                    Atribuicoes = "Modelar banco de dados.",
+                    ValoresAtitudes = "Estimular a organização.\n" + 
+                                      "Promover ações que considerem o respeito às normas estabelecidas.\n" + 
+                                      "Fortalecer a persistência e o interesse na resolução de situações-problema.",
+                    Competencias = "Desenvolver modelo de banco de dados.",
+                    Habilidades = "Levantar as necessidades de informações do sistema.\n" + 
+                                  "Normalizar tabelas de banco de dados.\n" + 
+                                  "Associar tabelas para construção de banco de dados.\n" + 
+                                  "Aplicar linguagem SQL na construção de tabelas.",
+                    BasesTecnologicas = "Estrutura de banco de dados \n" +
+                                        "Modelo conceitual \n" +
+                                        "Modelo lógico\n" +
+                                        "Dicionário de dados\n" +
+                                        "Grau de cardinalidade\n" +
+                                        "Tipos de restrições de integridade e conceitos\n" +
+                                        "Conceitos de autorrelacionamento\n" +
+                                        "Normalização de tabelas\n" +
+                                        "Especialização e generalização (superclasses e subclasses, supertipo e subtipos)\n" +
+                                        "Conceito de domínio\n" +
+                                        "Conceito de tabelas\n" +
+                                        "Construção de projeto lógico de banco de dados\n" +
+                                        "Introdução ao SGBD SQL Server\n" +
+                                        "Interface de comando\n" +
+                                        "Comandos da ferramenta x comandos SQL\n" +
+                                        "Introdução a DDL"
 
                 };
 
@@ -128,13 +173,23 @@ namespace ProjetoCurso.View
             {
                 var c = new Componente
                 {
-                    Nome = "",
-                    CargaHoraria = 0,
-                    Atribuicoes = "",
-                    ValoresAtitudes = "",
-                    Competencias = "",
-                    Habilidades = "",
-                    BasesTecnologicas = ""
+                    Nome = "Análise e Projeto de Sistemas",
+                    Atribuicoes = "Elaborar projetos de sistema de informação.",
+                    ValoresAtitudes = "Incentivar a criatividade, \n" +
+                                      "Estimular a organização.",
+                    Competencias = "Modelar projeto de sistemas, \n" +
+                                   "Selecionar modelos para o desenvolvimento de sistemas.",
+                    Habilidades = "Coletar requisitos de usuários e sistemas, \n" +
+                                  "Utilizar métodos de abordagem e coleta de dados e procedimentos de pesquisa, \n " +
+                                  "Aplicar modelo Cascata no desenvolvimento de sistemas,\n" +
+                                  "Aplicar modelos Ágeis a projetos de software.",
+                    BasesTecnologicas = "Introdução e conceitos básicos de análise de sistemas e projetos. \r\n" +
+                    "Ciclo de vida de um sistema: \n" +
+                    "   - Estudo de viabilidade.\n" +
+                    "   - Especificação de requisitos.\n" +
+                    "   - Concepções do modelo Cascata.\n" +
+                    "   - Concepções dos modelos Ágeis.\n" +
+                    "   - Introdução à análise e projeto orientado a objetos."
 
                 };
 
@@ -152,13 +207,25 @@ namespace ProjetoCurso.View
             {
                 var c = new Componente
                 {
-                    Nome = "",
-                    CargaHoraria = 0,
-                    Atribuicoes = "",
-                    ValoresAtitudes = "",
-                    Competencias = "",
-                    Habilidades = "",
-                    BasesTecnologicas = ""
+                    Nome = "Design Digital",
+                    Atribuicoes = "Desenvolver interfaces visuais para aplicativos e sites,\n" +
+                                  "Desenvolver elementos gráficos para aplicativos e sites.",
+                    ValoresAtitudes = "Incentivar a criatividade,\n" + 
+                                      "Incentivar ações que promovam a cooperação,\n" +
+                                      "Respeitar as manifestações culturais de outros povos.",
+                    Competencias = "Desenvolver interfaces visuais para aplicativos e sites.",
+                    Habilidades = "Manipular e/ou construir elementos visuais para aplicativos e sites.",
+                    BasesTecnologicas = "Conceitos de Design Gráfico aplicado à construção de aplicativos e sites\n" + 
+                                        "Teoria das cores, tipografia e composição\n" + 
+                                        "Ferramentas de seleção e manipulação de objetos\n" +
+                                        "Ferramentas de edição e tratamento\n" + 
+                                        "Transformação de objeto\n" + 
+                                        "Timeline, máscara e mesclagem de camadas\n" + 
+                                        "Ferramentas de texto\n" + 
+                                        "Estilo e filtros de imagens\n" + 
+                                        "Manipulação de documentos\n" + 
+                                        "Recursos para a criação/manipulação de imagens para a construção de botões, banners, logomarca\n" +
+                                        "Regras (Heurísticas) de usabilidade"
 
                 };
 
--- ProjetoCurso/ProjetoCurso/View/SegundoAno.xaml.cs
+++ ProjetoCurso/ProjetoCurso/View/SegundoAno.xaml.cs
@@ -24,13 +24,20 @@ namespace ProjetoCurso.View
             {
                 var c = new Componente
                 {
-                    Nome = "",
-                    CargaHoraria = 0,
-                    Atribuicoes = "",
-                    ValoresAtitudes = "",
-                    Competencias = "",
-                    Habilidades = "",
-                    BasesTecnologicas = ""
+                    Nome = "Programação Web II",
+                    Atribuicoes = "Planejar projetos de sistemas de informação para Web.",
+                    ValoresAtitudes = "Incentivar a criatividade.\r\n" + 
+                                      "Responsabilizar-se pela produção, utilização e divulgação de informações.\r\n" + 
+                                      "Fortalecer a persistência e o interesse na resolução de situações-problema.",
+                    Competencias = "Desenvolver sistemas para internet utilizando persistência em banco de dados, interface com o usuário e programação em lado servidor.",
+                    Habilidades = "Codificar software em linguagem para Web.\r\n" + 
+                                  "Utilizar banco de dados relacionais para persistência dos dados.\r\n" + 
+                                  "Utilizar interface baseada em navegador para interação com usuário.",
+                    BasesTecnologicas = "Introdução a scripts lado servidor\n" +
+                                        "Variáveis e tipos de dados\n" +
+                                        "Comunicação entre navegador e aplicação\n" +
+                                        "Persistência em banco de dados\n" +
+                                        "Modularização e organização dos programas \n"
 
                 };
 
@@ -48,13 +55,27 @@ namespace ProjetoCurso.View
             {
                 var c = new Componente
                 {
-                    Nome = "",
-                    CargaHoraria = 0,
-                    Atribuicoes = "",
-                    ValoresAtitudes = "",
-                    Competencias = "",
-                    Habilidades = "",
-                    BasesTecnologicas = ""
+                    Nome = "Banco de Dados II",
+                    Atribuicoes = "Implementar banco de dados.",
+                    ValoresAtitudes = "Incentivar a criatividade.\r\n" + 
+                                      "Desenvolver a criticidade.\r\n" + 
+                                      "Fortalecer a persistência e o interesse na resolução de situações-problema.",
+                    Competencias = "Implementar banco de dados relacional utilizando o Sistema Gerenciador de banco de dados.\r\n" + 
+                                   "Otimizar a linguagem de consulta estruturada como forma de informação relevante para a tomada de decisão",
+                    Habilidades = "Utilizar sistema de gerenciamento para banco de dados.\r\n" + 
+                                  "Executar linguagem de consulta estruturada objetivando melhor desempenho.\r\n" + 
+                                  "Compilar relatórios analíticos a partir dos dados coletados",
+                    BasesTecnologicas = "Implementação de banco de dados\r\n" + 
+                                        "Criação e exclusão de banco de dados. \r\n" + 
+                                        "Variáveis e constantes\r\n" + 
+                                        "Conceitos e utilização.\r\n" + 
+                                        "Comandos SQL\r\n" + 
+                                        "Linguagem de definição de dados – DDL.\r\n" + 
+                                        "Linguagem de manipulação de dados – DML\r\n" + 
+                                        "Linguagem de consulta de dados – DQL\r\n" + 
+                                        "Blocos de linguagem de consulta estruturada (SQL)\r\n" + 
+                                        "Exceções (tratamento de erros)\r\n" + 
+                                        "Funções\r\n" + "Gatilhos\r\n" + "Visões controladas\r\n" + "Índices\r\n" + "Merge e permissões"
 
                 };
 
@@ -72,9 +93,9 @@ namespace ProjetoCurso.View
             {
                 var c = new Componente
                 {
-                    Nome = "",
-                    CargaHoraria = 0,
-                    Atribuicoes = "",
+                    Nome = "Desenvolvimento de Sistemas",
+                    Atribuicoes = "Codificar e depurar programas.\r\n" + 
+                    "Selecionar linguagens de programação e ambientes de desenvolvimento de acordo com as \r\nespecificidades do projeto.",
                     ValoresAtitudes = "",
                     Competencias = "",
                     Habilidades = "",
